b229776fb73a07df65e431159fcb68f2264d39b6
Jeff <jeff.jenkinson@gmail.com>
2022-12-06 15:16:54 +0200

    Fix repo links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,13 +37,13 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/trixtateam/docs/",
+          editUrl: "https://github.com/trixtateam/docs/blob/master/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/trixtateam/docs/",
+          editUrl: "https://github.com/trixtateam/docs/blob/master/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
